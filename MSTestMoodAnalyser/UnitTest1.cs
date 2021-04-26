6568f6820f6dd2db75f10d4dfd6c84ee1241995c
Patil-94 <ankitakhairnar10@.com>
2021-04-26 15:20:13 +0530

    [Ankita] Add:Reflection To Change Mood Dynamically

--- MSTestMoodAnalyser/UnitTest1.cs
+++ MSTestMoodAnalyser/UnitTest1.cs
@@ -80,6 +80,50 @@ namespace MSTestMoodAnalyser
                 Assert.AreEqual(MoodAnalyserException.ExceptionType.INVALID_INPUT, e.Message);
             }
         }
+
+        /// <summary>
+        /// Test Case 7.1
+        /// set Happy message with Reflector should return HAPPY
+        /// </summary>
+        [TestMethod]
+        public void WhenHappyMessage_ShouldReturnHappy()
+        {
+            dynamic result = MoodAnalyserFactory.ChangeMoodDynamically("MoodAnalyser.MoodAnalyserMain", "HAPPY");
+            Assert.AreEqual("HAPPY", result);
+        }
+
+        /// <summary>
+        /// Test Case 7.2 set field when improper should throw Exception 
+        /// </summary>
+        [TestMethod]
+        public void WhenImproperMessage_ShouldThrowException()
+        {
+            try
+            {
+                string message = MoodAnalyserFactory.ChangeMoodDynamically("MoodAnalyzer.getMood", "HAPPY");
+            }
+            catch (MoodAnalyserException e)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.INVALID_INPUT, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Test Case 7.3 setting Null message with Reflector should throw Exception
+        /// </summary>
+        [TestMethod]
+        public void WhenNull_ShouldThrowException()
+        {
+            try
+            {
+                dynamic result = MoodAnalyserFactory.ChangeMoodDynamically("MoodAnalyzer.MoodAnalyser", null);
+            }
+            catch (MoodAnalyserException e)
+            {
+                Assert.AreEqual(MoodAnalyserException.ExceptionType.NULL_EXCEPTION, e.Message);
+            }
+        }
     }
 }
+
 
--- MoodAnalyserProblem/MoodAnalyserFactory.cs
+++ MoodAnalyserProblem/MoodAnalyserFactory.cs
@@ -77,8 +77,31 @@ namespace MoodAnalyserProblem
                 throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.INVALID_INPUT, "No Such Method");
             }
         }
+
+        /// <summary>
+        /// UC7 Use Reflection to change mood dynamically
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="mood"></param>
+        /// <returns></returns>
+        public static dynamic ChangeMoodDynamically(string className, string mood)
+        {
+            try
+            {
+                Type type = Type.GetType(className);
+                dynamic change_mood = Activator.CreateInstance(type, mood);
+                MethodInfo method = type.GetMethod("GetMood");
+                dynamic value = method.Invoke(change_mood, new object[] { mood });
+                return value;
+            }
+            catch (Exception e)
+            {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL_EXCEPTION, e.Message);
+            }
+        }
     }
 }
+
 
 
     
